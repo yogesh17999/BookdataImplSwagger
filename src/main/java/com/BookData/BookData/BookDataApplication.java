7e0dc29ed26a811110d4c9fdbf2aad3d1aeec633
yogesh17999 <yogesh17999@gmail.com>
2023-05-05 14:32:09 +0530

    New project creted Bookdata implement in swagger

---
+++ src/main/java/com/BookData/BookData/BookDataApplication.java
@@ -0,0 +1,13 @@
+package com.BookData.BookData;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookDataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/BookData/BookData/BookDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BookData.BookData;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
